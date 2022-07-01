d994081908619442541735723ff7e5c62a346bc8
Luiz Gustavo V. matos <contatoluizgustavoig@gmail.com>
2022-07-01 15:15:27 -0300

    complemento cliente

--- Fisica.java
+++ Fisica.java
@@ -8,8 +8,24 @@ public class Fisica extends Cliente {
 		super (novonome, novoid, novaidade);
 		this.cpf = cpf;
 	}
-	
-	
+	 public  void relatorio ()
+	    {
+	    	System.out.println("-------------------------");
+	    	System.out.println("Cliente:");
+	    	System.out.println("Nome:" +nome);
+	    	System.out.println("Id:" +id);
+	    	System.out.println("Idade:" +idade);
+	    	System.out.println("Cpf:" +cpf);
+	    	System.out.println("-------------------------");
+	       
+	    }
+	    @Override
+		public String toString() {
+			return  this.nome 
+					+ " | ID: " + this.id
+					+ " | Idade: " + this.idade
+					+ " | Cpf: " +this.cpf;
+		}
 	
 	
 	
