5099b84defde67f19e35951daea92e6526559898
Luiz Gustavo V. matos <contatoluizgustavoig@gmail.com>
2022-07-02 22:38:05 -0300

    inicio do projeto em javaFX

---
+++ AppController.java
@@ -0,0 +1,61 @@
+package TrabalhoFinal_LP1;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class AppController {
+
+	private Stage janela;
+	private Scene cena;
+	private Parent raiz;
+	
+	//public void inserir() throws IOException {
+		//FXMLLoader loader = new FXMLLoader(getClass().getResource("CenaImposto.fxml"));
+		//raiz = loader.load();
+		//Repositorio repositorio = loader.getController();
+		//repositorio.inserir(Imposto c);
+	//}
+	
+	public void LoginFisico(ActionEvent e) throws IOException {
+		raiz = FXMLLoader.load(getClass().getResource("ClienteFisico.fxml"));
+		janela = (Stage)((Node)e.getSource()).getScene().getWindow();
+		Scene cena = new Scene(raiz);
+		janela.setScene(cena);
+		janela.show();
+	}
+	public void LoginJuridico(ActionEvent e) throws IOException {
+		raiz = FXMLLoader.load(getClass().getResource("ClienteJuridico.fxml"));
+		janela = (Stage)((Node)e.getSource()).getScene().getWindow();
+		Scene cena = new Scene(raiz);
+		janela.setScene(cena);
+		janela.show();
+	}
+	public void LoginImposto(ActionEvent e) throws IOException {
+		raiz = FXMLLoader.load(getClass().getResource("CenaImposto.fxml"));
+		janela = (Stage)((Node)e.getSource()).getScene().getWindow();
+		Scene cena = new Scene(raiz);
+		janela.setScene(cena);
+		janela.show();
+	}
+	public void LoginImposto2(ActionEvent e) throws IOException {
+		raiz = FXMLLoader.load(getClass().getResource("CenaImposto2.fxml"));
+		janela = (Stage)((Node)e.getSource()).getScene().getWindow();
+		Scene cena = new Scene(raiz);
+		janela.setScene(cena);
+		janela.show();
+	}
+	public void CenaInserir(ActionEvent e) throws IOException {
+		raiz = FXMLLoader.load(getClass().getResource("CenaInserir.fxml"));
+		janela = (Stage)((Node)e.getSource()).getScene().getWindow();
+		Scene cena = new Scene(raiz);
+		janela.setScene(cena);
+		janela.show();
+	}
+}
---
+++ app.java
@@ -0,0 +1,31 @@
+package TrabalhoFinal_LP1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class app extends Application {
+	@Override
+	public void start(Stage janela) {
+		try {
+			Parent raiz = FXMLLoader.load(getClass().getResource("MenuInicial.fxml"));
+			Scene cena = new Scene(raiz, Color.LIGHTBLUE);
+			janela.setScene(cena);
+			janela.setResizable(false);
+			Image Icone = new Image("icon.png");
+			janela.getIcons().add(Icone);
+			janela.setTitle("Seu Imposto");
+			janela.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
